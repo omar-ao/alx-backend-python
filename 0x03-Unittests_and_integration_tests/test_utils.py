e764839dce516b67f4db7e88ef9977e09b420df0
Omar Ahmed O <deeyndiid@gmail.com>
2024-11-04 20:20:55 +0300

    Add task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,11 @@ Test Module
 """
 import unittest
 from parameterized import parameterized
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+)
 from utils import *
 
 
@@ -16,7 +21,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ("test_2", {"a": {"b": 2}}, ("a",), {'b': 2}),
         ("test_3", {"a": {"b": 2}}, ("a", "b"), 2),
         ])
-    def test_access_nested_map(self, name, a, b, expected):
+    def test_access_nested_map(self, name: str, a: Mapping,
+                               b: Sequence, expected: Any) -> None:
         """
         test_access_nested_map
         """
